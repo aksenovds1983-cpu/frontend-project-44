0a2d7cd40c47d7eeab05e1e977510b2c0099ed0a
Dmitriy <asenov.ds1983@gmail.com>
2026-03-19 21:41:39 +0700

    step six

---
+++ src/games/calc.js
@@ -0,0 +1,37 @@
+import runGame from '../index.js';
+
+const operators = ['+', '-', '*'];
+
+const calculate = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error(`Unknown operator: ${operator}`);
+  }
+};
+
+const generateRound = () => {
+  const num1 = Math.floor(Math.random() * 20) + 1;
+  const num2 = Math.floor(Math.random() * 20) + 1;
+  
+  const operator = operators[Math.floor(Math.random() * operators.length)];
+  
+  const question = `${num1} ${operator} ${num2}`;
+  
+  const correctAnswer = calculate(num1, num2, operator).toString();
+  
+  return { question, correctAnswer };
+};
+
+const gameRules = 'What is the result of the expression?';
+
+const playCalcGame = () => {
+  runGame(gameRules, generateRound);
+};
+
+export default playCalcGame;
--- src/games/even.js
+++ src/games/even.js
@@ -1,39 +1,21 @@
-// src/games/even.js
-import readlineSync from 'readline-sync';
+import runGame from '../index.js';
 
 const isEven = (num) => num % 2 === 0;
 
+const generateRound = () => {
+  const number = Math.floor(Math.random() * 100) + 1;
+  
+  const question = number.toString();
+  
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  
+  return { question, correctAnswer };
+};
+
+const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+
 const playEvenGame = () => {
-  console.log('Welcome to the Brain Games!');
-  
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  
-  const roundsToWin = 3;
-  
-  let correctAnswers = 0;
-  
-  while (correctAnswers < roundsToWin) {
-    const number = Math.floor(Math.random() * 100) + 1;
-    
-    console.log(`Question: ${number}`);
-    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-    
-    const correctAnswer = isEven(number) ? 'yes' : 'no';
-    
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-      correctAnswers++;
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return; 
-    }
-  }
-  
-  console.log(`Congratulations, ${name}!`);
+  runGame(gameRules, generateRound);
 };
 
 export default playEvenGame;
